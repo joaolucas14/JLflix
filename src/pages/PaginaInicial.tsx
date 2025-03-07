a90f6b026d58f25e464d76fdacbcc9eb3b93ae25
joaolucas14 <jlparceiro10@gmail.com>
2025-03-07 13:18:50 -0300

    iniciando os testes com as buscas da api

--- src/pages/PaginaInicial.tsx
+++ src/pages/PaginaInicial.tsx
@@ -1,7 +1,45 @@
+import { useEffect, useState } from "react";
+import http from "../api";
+
 export default function PaginaInicial() {
+  const [filmes, setFilmes] = useState();
+  useEffect(() => {
+    async function buscarFilmes() {
+      try {
+        const resposta = await http.get("discover/movie");
+        console.log(resposta.data);
+        setFilmes(resposta.data);
+      } catch (erro) {
+        console.error("Erro ao conectar à API:", erro);
+      }
+    }
+
+    buscarFilmes(); // Chamando a função dentro do useEffect
+  }, []);
+
   return (
     <div>
-      <h1>Pagina Inicial</h1>
+      <h1>Página Inicial</h1>
+      {filmes &&
+        filmes.results.map((filme: any) => (
+          <div
+            key={filme.imdbID}
+            style={{ margin: "20px", textAlign: "center" }}
+          >
+            <p>{filme.id}</p>
+            <p>{filme.title}</p>
+            <img
+              src={`https://image.tmdb.org/t/p/original${filme.poster_path}`}
+              alt={filme.title}
+              style={{
+                width: "200px",
+                height: "300px",
+                objectFit: "cover",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+              }}
+            />
+          </div>
+        ))}
     </div>
   );
 }
