23366db037d16f1de3cc54d88d02ce41a4856d0a
joaolucas14 <jlparceiro10@gmail.com>
2025-03-13 11:02:59 -0300

    remove unused cast list div and add padding to film page layout

--- src/components/ListCast/index.tsx
+++ src/components/ListCast/index.tsx
@@ -5,7 +5,6 @@ export default function ListCast({ cast }: ICreditos) {
   return (
     <div className={styles.cast}>
       <h2>Elenco</h2>
-      <div className={styles.cast_list}></div>
       {cast.slice(0, 5).map((ator) => (
         <div key={ator.id} className={styles.cast_list}>
           <p className={styles.cast_name}>
