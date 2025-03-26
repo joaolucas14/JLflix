e062074ec9fd937ccc1e332a3cf620de596b0308
joaolucas14 <jlparceiro10@gmail.com>
2025-03-26 11:49:16 -0300

    adiciona estilos e estrutura ao componente ListCast, incluindo exibição de elenco com imagens e informações

--- src/components/ListCast/index.tsx
+++ src/components/ListCast/index.tsx
@@ -1,24 +1,27 @@
 import { ICreditos } from "../../interfaces/ICreditos";
-import ContainerInfoMovies from "../ContainerInfoMovies";
 import styles from "./ListCast.module.css";
 
 export default function ListCast({ cast }: ICreditos) {
   return (
-    <ContainerInfoMovies titulo="Atores" direita={true}>
-      {cast.slice(0, 5).map((ator) => (
-        <div key={ator.id} className={styles.cast_list}>
-          <p className={styles.cast_name}>
-            {`${ator.name}: ${ator.character}`}
-          </p>
-          {ator.profile_path && (
-            <img
-              src={`https://image.tmdb.org/t/p/w200${ator.profile_path}`}
-              alt={ator.name}
-              className={styles.cast_image}
-            />
-          )}
-        </div>
-      ))}
-    </ContainerInfoMovies>
+    <>
+      {" "}
+      <h2>Elenco</h2>
+      <div className={styles.container}>
+        {cast
+          .filter((ator) => ator.profile_path)
+          .slice(0, 15)
+          .map((ator) => (
+            <div key={ator.id} className={styles.cast_list}>
+              <img
+                src={`https://image.tmdb.org/t/p/w200${ator.profile_path}`}
+                alt={ator.name}
+                className={styles.cast_image}
+              />
+              <p className={styles.actor}>{`${ator.name} `}</p>
+              <p className={styles.character}>{ator.character}</p>
+            </div>
+          ))}
+      </div>
+    </>
   );
 }
